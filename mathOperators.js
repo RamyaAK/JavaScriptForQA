6869dc61caa1adaa0d6ef6935e2f9f2328d7dbe9
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-27 19:16:57 +0530

    Added basic MathOperators code

---
+++ mathOperators.js
@@ -0,0 +1,28 @@
+// Math Operators:  +,-,*,/,%,**,++,--
+
+var a=10
+var b=5
+//Addition
+console.log(a+b)
+//Subtraction
+console.log(a-b)
+//Multiplication
+console.log(a*b)
+//Division
+console.log(a/b)
+//modulo or remainder
+console.log(a%b)
+//a to the Power of b
+console.log(a**b)
+//Post Increment Operator
+console.log(a++)
+//Post decrement Operator
+console.log(b--)
+
+a=10
+b=5
+//Pre Increment Operator
+console.log(++a)
+//Pre decrement Operator
+console.log(--b)
+
