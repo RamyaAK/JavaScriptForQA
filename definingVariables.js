98435c53a763c101f5b188a2b55a22d547d3b80e
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-27 16:49:42 +0530

    Initail Commit with a readme and a Variables file

---
+++ definingVariables.js
@@ -0,0 +1,21 @@
+var book1 ="Bee in the Hive"
+var book2 ="The Secret Life of the Ant"
+var book3 ="Laptops. Not just for breakfast anymore"
+var book4 ="If I were a Superhero"
+var book5 ="Once upon a time in the Rabbit Hole"
+var book6 ="In memory of Last Slice of Pizza"
+var book7 ="Who Stole Second Base?"
+var book8 ="The Coding Odyssey"
+var book9 ="T"
+var book10= "The End"
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
+console.log(book4);
+console.log(book5);
+console.log(book6);
+console.log(book7);
+console.log(book8);
+console.log(book9);
+console.log(book10);
