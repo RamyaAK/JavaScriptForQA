6fc50125bf5327218a4020ae1ebbf30a246941f6
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-28 12:21:37 +0530

    Adding prompt and alert code example

---
+++ promptAlert.js
@@ -0,0 +1,5 @@
+// Run this code in jsconsole.com
+
+var answer = prompt("Enter your Name"); 
+console.log("Hello "+answer+"! Hope you are doing good!")
+alert("Hello "+answer+"! Hope you are doing good")
