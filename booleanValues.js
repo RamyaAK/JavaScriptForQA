a190e030f88ee8068c4aa80d0872416677ce0138
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-01 22:53:02 +0530

    boolean values example

---
+++ booleanValues.js
@@ -0,0 +1,17 @@
+var areYouHappy = true
+
+if(areYouHappy == true)
+{
+   console.log("I'm so glad that u are happy too!")
+}
+else{
+    console.log("Please do something that makes you happy!")
+}
+
+var areYouFake = false
+
+if (areYouFake == false){
+    console.log("I know that you are a real gem kind of person. You are as real as the Sun and the Moon.")
+}else{
+    console.log("practice being more real and not to fake everything that you do")
+}
---
+++ giftIdeasForMyWife.js
@@ -0,0 +1,31 @@
+// I have come up with few of the gift ideas for my wife but I need your help in deciding one best gift amongst these.
+
+var answer = prompt("What do you think would be a perfect gift to buy for my wife among these=>roses,jewelry,card, money, candy,electronics, books, or a puppy ?");
+
+if(answer =="roses"){
+    console.log(answer+" would be a perfect birthday present if your wife loves flowers!");
+}
+else if(answer =="jewelry"){
+    console.log(answer+"would be a perfect birthday present if your wife is fond of wearing beautiful accessories!");
+}
+else if(answer =="card"){
+    console.log("A lovely handmade "+answer+" would be a perfect birthday present if your wife and she will always keep it with her!");
+}
+else if(answer =="money"){
+    console.log("Simply gift her some "+answer+" so that she would buy anything or spend it the way she loves on her birthday!");
+}
+else if(answer =="candy"){
+    console.log("Simply gift her favorite "+answer+" on her birthday so that her day becomes as sweet as candy!");
+
+}
+else if(answer =="electronics"){
+    console.log("An "+answer+" gadget like a wrist band, phone, headsets, powerbank, electrical musical instrument that your wife can use in day to day life would be a great choice of gift!");
+}
+else if(answer =="books"){
+    console.log("Gifting a "+answer+" to your nerdy wife is like gifting her a new best friend a companion who she would love to spend time reading.");
+}
+else if(answer =="puppy"){
+    console.log("who would not love to have a cute lil "+answer+" as a pet? I think this can be the best gift for your wife on her special day!");
+}
+
+
