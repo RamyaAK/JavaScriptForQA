bb4f7b0db993e7cef58cdb8dbd25175cd261f836
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-01 23:19:20 +0530

    JS Arrays an daccessing nested item value in an array example.

---
+++ arrayExample.js
@@ -0,0 +1,16 @@
+var container =[1,true,"Y","N","Ramya",0.6,[1,2,3],["Ramya","Rashmi"],"heart",0]
+var first ="Ram"
+var second ="Sita"
+var third ="Hanuman"
+
+var ramayanCharacters =["Ram","Lakshman","Sita","Ravana","Hanuman"]
+
+console.log(ramayanCharacters)
+console.log(container)
+console.log(first,second,third)
+
+// if i want to get the inner array of container value 2 then we can use
+console.log(container[6][1]) //2
+console.log(container[5])
+console.log(container[7][1])
+console.log(container.length)
