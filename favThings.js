ec1c60605dbfcb50100924b8cf02a1aacfac4b60
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-02 21:50:33 +0530

    JS Array favThings Activity

---
+++ favThings.js
@@ -0,0 +1,21 @@
+// make a list of your fav things
+
+var things =[]
+
+var answer = prompt("Enter your first most fav thing in this world")
+things.push(answer)
+
+var answer = prompt("Enter your second most fav thing in this world")
+things.push(answer)
+
+var answer = prompt("Enter your third most fav thing in this world")
+things.push(answer)
+
+var answer = prompt("Enter your fourth most fav thing in this world")
+things.push(answer)
+
+var answer = prompt("Enter your fifth most fav thing in this world")
+things.push(answer)
+
+console.log(things)
+
