7e7ebbc0b1c574e8f3bd654eb8fa7876d212b4cc
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-02 17:23:07 +0530

    JS Arrays methods push and pop example.

---
+++ popAndPushInArray.js
@@ -0,0 +1,13 @@
+// push -- adds an item into the Array
+// pop -- reoves the last item from the array
+
+var container =["red","green","blue"]
+
+container.push("Yellow") //add Yellow
+console.log(container)
+
+container.push("Purple") // add Purple
+container.push("Pink")  // add Pink
+console.log(container)
+
+console.log(container.pop()) // deletes Pink
