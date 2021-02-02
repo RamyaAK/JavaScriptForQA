0425f6115e67b3b9e855e5a26ce646eee2383d47
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-02 22:31:00 +0530

    Added break usages to stop forLoop  in the JS

---
+++ breakingForLoops.js
@@ -0,0 +1,10 @@
+// use the break keyword in js to come out of the loop and stop looping.
+
+for(var i =0;i<=10;i++){
+    if(i==5){
+        break;
+    }
+    else{
+        console.log(i)
+    }
+} 
