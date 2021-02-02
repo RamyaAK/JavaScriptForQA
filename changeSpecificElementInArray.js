45505d57c77cd9ea5aed93cfef20cd8af0f7368d
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-02 20:59:37 +0530

    JS Array changing a specific item  example.

---
+++ changeSpecificElementInArray.js
@@ -0,0 +1,12 @@
+var listItems = ["book","pen","ruler","pencil"]
+
+listItems[2]="eraser" // edit the value in array using its index value. 
+
+console.log(listItems)
+
+listItems[4]="bag" // A new item gets created if in case the index is more than given listItem size.
+console.log(listItems)
+
+listItems[6]="lunchbox" // adds in the index loc given and keeps other index as undefined.
+console.log(listItems) 
+
