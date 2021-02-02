155bcb0a99746a771235fbd414a3e151bd3bb994
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-02 20:49:40 +0530

    JS Arrays methods shift and unshift example.

---
+++ shiftAndUnshiftArray.js
@@ -0,0 +1,11 @@
+//shift - delete first element from the array
+//unshift - adds element in the first index of the array.
+
+var container =["red","blue","green"]
+container.shift()
+console.log(container)
+
+
+var container =["red","blue","green"]
+container.unshift("purple")
+console.log(container)
