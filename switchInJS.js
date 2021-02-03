1d01f18f6933351980e8127d1d976d8c1136d1e7
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-03 15:50:24 +0530

    Added an example for Switch stmt

---
+++ switchInJS.js
@@ -0,0 +1,24 @@
+//fav food
+var favFood = "Gobi65"
+
+switch(favFood){
+    case "Sandwitch" : console.log("Your fav food is sandwitch")
+    break
+    case "Dosa" : console.log("Your fav food is Dosa")
+    break
+    case "Idli" : console.log("Your fav food is Idli")
+    break
+    case "Bisibelebath" : console.log("Your fav food is Bisibelebath")
+    break
+    case "Pizza" : console.log("Your fav food is Pizza")
+    break
+    case "Gobi65" : console.log("Your fav food is Gobi65")
+    break
+    case "Burger" : console.log("Your fav food is Burger")
+    break
+    case "FrenchFries" : console.log("Your fav food is FrenchFries")
+    break
+    case "DeathByChocolate" :  console.log("Your fav food is DeathByChocolate")
+    break
+    default : console.log("I like everything, All food are my favorite")
+}
