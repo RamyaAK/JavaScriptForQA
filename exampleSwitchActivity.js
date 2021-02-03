4f28cd1a1ae455060d223f1ecd82659a3947581f
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-03 16:28:32 +0530

    Added switch example, Added basic function usage in JS

---
+++ exampleSwitchActivity.js
@@ -0,0 +1,16 @@
+var favColor = "red"
+
+switch(favColor){
+case "blue":
+console.log("Violets are blue")
+break;
+
+case "red":
+console.log("Roses are red")
+break;
+
+default: console.log("Sugar is sweet")
+break;
+}
+
+
---
+++ functions.js
@@ -0,0 +1,9 @@
+// functions: reusable blocks that can be called as many times as needed.
+
+function printHello(){
+   console.log("Hello there!")
+}
+
+printHello()
+printHello()
+printHello()
