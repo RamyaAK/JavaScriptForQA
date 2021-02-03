1465556f0bad2824ba780e62ff99aa6bf5dfcf6a
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-03 23:04:02 +0530

    Updated a simple cashier example

--- simpleCashier.js
+++ simpleCashier.js
@@ -14,22 +14,48 @@
 
 //     You gave too little. You still owe $x.xx.
 
-var totalMoney = 1000
-var userPay = prompt("Please enter the amount that you wish to pay now. You actually owe $1000")
-amount = parseInt(userPay)
-function simpleCashier(amount) {
-    var amount_due;
-    amount_due = totalMoney - amount;
-    return amount_due;
+// var totalMoney = 1000
+// var userPay = prompt("Please enter the amount that you wish to pay now. You actually owe $1000")
+// amount = parseInt(userPay)
+// function simpleCashier(amount) {
+//     var amount_due;
+//     amount_due = totalMoney - amount;
+//     return amount_due;
+// }
+
+// var result = simpleCashier(amount);
+// if(result == 0){
+//     console.log("You gave the exact amount.")
+// }
+// else if(result<=totalMoney){
+//    console.log("You gave too little. You still owe $"+result)
+// }
+// else if(result >= totalMoney){
+//     console.log("You gave too much. Your change due is $"+result)
+// }
+
+
+
+
+var owe = 3450
+
+var payment = prompt("You owe $" + owe + ". How much do you want to pay?")
+
+
+function paymentMng(originalCost, moneyIn) {
+
+remaining = originalCost - moneyIn
+
+return remaining
+
 }
 
-var result = simpleCashier(amount);
-if(result == 0){
-    console.log("You gave the exact amount.")
-}
-else if(result<=totalMoney){
-   console.log("You gave too little. You still owe $"+result)
-}
-else if(result >= totalMoney){
-    console.log("You gave too much. Your change due is $"+result)
-}
+var remaining = paymentMng(owe, payment)
+
+console.log(remaining)
+
+if (remaining<0){ console.log("You gave too much. Your change due is $" + Math.abs(remaining))}
+
+if (remaining==0){ console.log("You gave the exact amount")}
+
+if (remaining>0){ console.log("You gave too little. You still owe $" + remaining)}
