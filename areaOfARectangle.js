fdb4a6dcdc77cad3753a775bd069719d7399c0d5
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-03 17:53:43 +0530

    computing the Area of a rectangle

---
+++ areaOfARectangle.js
@@ -0,0 +1,12 @@
+// calculating the area of a rectangle.
+
+// function definition
+function computeAreaOfRectanglewith(length,breadth){ // length,breadth are the fn parameters
+    var area = length * breadth
+    return area
+}
+
+console.log(computeAreaOfRectanglewith(10,34)) // 10,34 are the fn arguments.
+console.log(computeAreaOfRectanglewith(2,4))
+console.log(computeAreaOfRectanglewith(100,40))
+console.log(computeAreaOfRectanglewith(10,20))
