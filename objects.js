eeb7c000333c38f32a4c08084cec2f31778588cc
RamyaK <ext_rakulkarni@falabella.cl>
2021-02-04 11:18:12 +0530

    Added objects codein JS

---
+++ objects.js
@@ -0,0 +1,18 @@
+var person ={
+    favName: "Rashmi",
+    favColor: "Blue",
+    favFood: "Pizza",
+    favNumber: 2,
+    isMarried: true,
+    children: ["child1","child2"],
+    favThing: {
+        thingName: "object",
+        color: "Orange"
+    }
+}
+
+console.log(person)
+console.log(person.isMarried)
+console.log(person.children)
+console.log(person.children[1])
+console.log(person.favThing.thingName)
